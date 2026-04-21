e561e2a23c013319f470a0dca3036a6b00793cc0
Mvpuknowme or Michael Vincent Patrick <130760115+MVPuknowme@users.noreply.github.com>
2026-04-21 00:50:40 -0700

    Update gen_protos.sh

--- scripts/gen_protos.sh
+++ scripts/gen_protos.sh
@@ -5,11 +5,4 @@ if [ ! -x "$(which protoc)" ]; then
   brew install swift-protobuf
 fi
 
-git submodule update --init --recursive
-
-git submodule foreach --recursive git pull origin master
-
-protoc --proto_path=./protobufs --swift_opt=Visibility=Public --swift_out=./MeshtasticProtobufs/Sources ./protobufs/meshtastic/*.proto 
-
-echo "Done generating the swift files from the proto files."
-echo "Build, test, and commit changes."
+end
